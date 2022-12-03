d4846b34d10ffe0ff0697a9b1f04f86751521c13
Herberth Ovidio Jurado Maradiaga <juradomaradiaga34@gmail.com>
2022-12-03 12:06:17 -0600

    Modifique el formulario
    agregue los campos de Dui, Correo y edad en el formulario, asignando nombre a los TextBox.
    
    nota: falta agregarlos en el codigo

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -36,17 +36,31 @@ Partial Class Form1
         Me.grpCodigo = New System.Windows.Forms.GroupBox()
         Me.txtCodigo = New System.Windows.Forms.TextBox()
         Me.btnGenerar = New System.Windows.Forms.Button()
+        Me.dgvMostrar = New System.Windows.Forms.DataGridView()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
+        Me.txtEdad = New System.Windows.Forms.TextBox()
+        Me.txtDui = New System.Windows.Forms.TextBox()
+        Me.lblCorreo = New System.Windows.Forms.Label()
+        Me.lblEdad = New System.Windows.Forms.Label()
+        Me.lblDui = New System.Windows.Forms.Label()
         Me.grpDatos.SuspendLayout()
         Me.grpCodigo.SuspendLayout()
+        CType(Me.dgvMostrar, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'grpDatos
         '
+        Me.grpDatos.Controls.Add(Me.txtCorreo)
+        Me.grpDatos.Controls.Add(Me.txtEdad)
+        Me.grpDatos.Controls.Add(Me.txtDui)
+        Me.grpDatos.Controls.Add(Me.lblCorreo)
+        Me.grpDatos.Controls.Add(Me.lblEdad)
+        Me.grpDatos.Controls.Add(Me.lblDui)
+        Me.grpDatos.Controls.Add(Me.dgvMostrar)
         Me.grpDatos.Controls.Add(Me.txtApellido)
         Me.grpDatos.Controls.Add(Me.txtDireccion)
         Me.grpDatos.Controls.Add(Me.txtGenero)
         Me.grpDatos.Controls.Add(Me.txtNombre)
-        Me.grpDatos.Controls.Add(Me.btnAgregar)
         Me.grpDatos.Controls.Add(Me.lblApellido)
         Me.grpDatos.Controls.Add(Me.lblGenero)
         Me.grpDatos.Controls.Add(Me.lblDireccion)
@@ -55,7 +69,7 @@ Partial Class Form1
         Me.grpDatos.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.grpDatos.Location = New System.Drawing.Point(27, 130)
         Me.grpDatos.Name = "grpDatos"
-        Me.grpDatos.Size = New System.Drawing.Size(748, 301)
+        Me.grpDatos.Size = New System.Drawing.Size(846, 385)
         Me.grpDatos.TabIndex = 3
         Me.grpDatos.TabStop = False
         Me.grpDatos.Text = "Datos Alumno"
@@ -90,7 +104,7 @@ Partial Class Form1
         '
         'btnAgregar
         '
-        Me.btnAgregar.Location = New System.Drawing.Point(438, 250)
+        Me.btnAgregar.Location = New System.Drawing.Point(493, 31)
         Me.btnAgregar.Name = "btnAgregar"
         Me.btnAgregar.Size = New System.Drawing.Size(75, 32)
         Me.btnAgregar.TabIndex = 5
@@ -145,10 +159,11 @@ Partial Class Form1
         '
         Me.grpCodigo.Controls.Add(Me.txtCodigo)
         Me.grpCodigo.Controls.Add(Me.btnGenerar)
+        Me.grpCodigo.Controls.Add(Me.btnAgregar)
         Me.grpCodigo.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.grpCodigo.Location = New System.Drawing.Point(25, 19)
         Me.grpCodigo.Name = "grpCodigo"
-        Me.grpCodigo.Size = New System.Drawing.Size(460, 103)
+        Me.grpCodigo.Size = New System.Drawing.Size(650, 103)
         Me.grpCodigo.TabIndex = 2
         Me.grpCodigo.TabStop = False
         Me.grpCodigo.Text = "Generar Codigo Alumno"
@@ -169,11 +184,67 @@ Partial Class Form1
         Me.btnGenerar.Text = "Generar Codigo"
         Me.btnGenerar.UseVisualStyleBackColor = True
         '
+        'dgvMostrar
+        '
+        Me.dgvMostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvMostrar.Location = New System.Drawing.Point(34, 244)
+        Me.dgvMostrar.Name = "dgvMostrar"
+        Me.dgvMostrar.Size = New System.Drawing.Size(778, 135)
+        Me.dgvMostrar.TabIndex = 10
+        '
+        'txtCorreo
+        '
+        Me.txtCorreo.Location = New System.Drawing.Point(520, 88)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(311, 24)
+        Me.txtCorreo.TabIndex = 16
+        '
+        'txtEdad
+        '
+        Me.txtEdad.Location = New System.Drawing.Point(520, 135)
+        Me.txtEdad.Name = "txtEdad"
+        Me.txtEdad.Size = New System.Drawing.Size(88, 24)
+        Me.txtEdad.TabIndex = 15
+        '
+        'txtDui
+        '
+        Me.txtDui.Location = New System.Drawing.Point(520, 44)
+        Me.txtDui.Name = "txtDui"
+        Me.txtDui.Size = New System.Drawing.Size(311, 24)
+        Me.txtDui.TabIndex = 14
+        '
+        'lblCorreo
+        '
+        Me.lblCorreo.AutoSize = True
+        Me.lblCorreo.Location = New System.Drawing.Point(443, 88)
+        Me.lblCorreo.Name = "lblCorreo"
+        Me.lblCorreo.Size = New System.Drawing.Size(55, 18)
+        Me.lblCorreo.TabIndex = 13
+        Me.lblCorreo.Text = "Correo"
+        '
+        'lblEdad
+        '
+        Me.lblEdad.AutoSize = True
+        Me.lblEdad.Location = New System.Drawing.Point(443, 135)
+        Me.lblEdad.Name = "lblEdad"
+        Me.lblEdad.Size = New System.Drawing.Size(42, 18)
+        Me.lblEdad.TabIndex = 12
+        Me.lblEdad.Text = "Edad"
+        '
+        'lblDui
+        '
+        Me.lblDui.AutoSize = True
+        Me.lblDui.Location = New System.Drawing.Point(443, 44)
+        Me.lblDui.Name = "lblDui"
+        Me.lblDui.Size = New System.Drawing.Size(33, 18)
+        Me.lblDui.TabIndex = 11
+        Me.lblDui.Text = "DUI"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(923, 539)
         Me.Controls.Add(Me.grpDatos)
         Me.Controls.Add(Me.grpCodigo)
         Me.Name = "Form1"
@@ -182,6 +253,7 @@ Partial Class Form1
         Me.grpDatos.PerformLayout()
         Me.grpCodigo.ResumeLayout(False)
         Me.grpCodigo.PerformLayout()
+        CType(Me.dgvMostrar, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -200,4 +272,11 @@ Partial Class Form1
     Friend WithEvents grpCodigo As GroupBox
     Friend WithEvents txtCodigo As TextBox
     Friend WithEvents btnGenerar As Button
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents txtEdad As TextBox
+    Friend WithEvents txtDui As TextBox
+    Friend WithEvents lblCorreo As Label
+    Friend WithEvents lblEdad As Label
+    Friend WithEvents lblDui As Label
+    Friend WithEvents dgvMostrar As DataGridView
 End Class
